0c3fbe7001246618fcd8a18321f9fd2055f484c6
Gautam Mekkat <Blue9@users.noreply.github.com>
2020-05-05 03:53:46 -0400

    Add 808s and melodies

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ Run control.py in another Terminal tab. This lets you control the pitch.
 
 Change pitch: drag slider.
 
-Change sound: in the windo opened by synth.py, click on the interpreter text
+Change sound: in the window opened by synth.py, click on the interpreter text
 box, and enter looper.setTable(SndTable('synth2.wav')) or looper.setTable(SndTable('synth.wav')).
+
+
+In synth.py there are three types of sounds, continuous, single, and repeating.
+Uncomment each one (comment the other two out) and make sure they work. The single sound
+does not change pitch (you would have to manually replay the sound with pitch shifted).
+The repeating sound does change pitch but not from user input (see note_changes()).
+
--- synth.py
+++ synth.py
@@ -1,11 +1,11 @@
 from pyo import *
 
-s = Server(duplex=0).boot()
+s = Server(duplex=1).boot()
 rec = OscReceive(port=9000, address=['/pitch'])
 rec.setValue("/pitch", 0.5)
 
+For continuous sounds (synths, leads)
 table = SndTable('synth.wav')
-
 looper = Looper(table=table,
                 pitch=rec['/pitch'] * 2,
                 start=2,
@@ -18,9 +18,47 @@ looper = Looper(table=table,
                 autosmooth=True,
                 mul=1
                 )
-
 stlooper = looper.mix(2).out()
+
+# ----
+
+# For single sounds (808s)
+# snd = SndTable('808.wav')
+# osc = TableRead(table=snd, freq=snd.getRate()).out()
+
+# ----
+
+# For repeating sounds (kicks, hi-hats, melodies)
+
+# note = Sig(65)
+# class Melody(EventInstrument):
+#     def __init__(self, **args):
+#         EventInstrument.__init__(self, **args)
+
+#         snd = SndTable('celesta.wav')
+#         self.filt = Looper(table=snd,
+#                            pitch=self.freq / 261.6,
+#                            start=0,
+#                            mode=0,
+#                            dur=self.dur,
+#                            xfade=0,
+#                            mul=1).out()
+
+# l = Events(instr=Melody,
+#            midinote=EventSeq([note]),
+#            beat=EventSeq([1], occurrences=inf), db=-20.0, bpm=171)
+# l.play()
+
+# def note_changes():
+#     while True:
+#         note.setValue(note.value + 1)
+#         time.sleep(1)
+
+# import threading
+# threading.Thread(target=note_changes).start()
+
+
+# ----
+
 s.start()
 s.gui(locals())
-while True:
-    pass
